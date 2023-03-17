3107c2e67bbe03ece7cb2bd15949065bc1801385
User <User@DESKTOP-P5G4SF9>
2023-03-17 19:00:11 +0900

    added MenuMenager.java

--- src/sangha/MenuManager.java
+++ src/sangha/MenuManager.java
@@ -3,11 +3,11 @@ package sangha;
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Phonenumber");
-		System.out.println("2. Delete Person");
-		System.out.println("3. Edit");
-		System.out.println("4. View");
-		System.out.println("5. Exit");
+		System.out.println("1. Add Vehicle Owner");
+		System.out.println("2. Delete Vehicle Owner");
+		System.out.println("3. Edit Vehicle Owner");
+		System.out.println("4. View Vehicle Owner");
+		System.out.println("5. Exit Vehicle owner");
 
 	}
 
