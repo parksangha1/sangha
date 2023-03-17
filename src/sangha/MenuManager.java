1a26706bfa910f0c4908d21c0e7ea9f7fde3ce6b
User <User@DESKTOP-P5G4SF9>
2023-03-17 20:11:59 +0900

    added MenuManager.java

--- src/sangha/MenuManager.java
+++ src/sangha/MenuManager.java
@@ -7,7 +7,7 @@ public class MenuManager {
 		System.out.println("2. Delete Vehicle Owner");
 		System.out.println("3. Edit Vehicle Owner");
 		System.out.println("4. View Vehicle Owner");
-		System.out.println("5. Exit Vehicle owner");
+		System.out.println("5. Exit Vehicle Owner");
 
 	}
 
