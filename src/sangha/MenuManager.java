74f94d5da8e7cdcc465c3b5073665e73d9f8e61e
User <User@DESKTOP-P5G4SF9>
2023-03-17 09:27:59 +0900

    removed hello.java

---
+++ src/sangha/MenuManager.java
@@ -0,0 +1,14 @@
+package sangha;
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Phonenumber");
+		System.out.println("2. Delete Person");
+		System.out.println("3. Edit");
+		System.out.println("4. View");
+		System.out.println("5. Exit");
+
+	}
+
+}
