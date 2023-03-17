59a983b3901eccc4dc7cddd62b6f8236a18bbaaf
User <User@DESKTOP-P5G4SF9>
2023-03-17 09:20:47 +0900

    added Menu.java

---
+++ src/sangha/Menu.java
@@ -0,0 +1,14 @@
+package sangha;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		System.out.println("1. ");
+		System.out.println("1. ");
+		System.out.println("1. ");
+		System.out.println("1. ");
+		System.out.println("1. ");
+
+	}
+
+}
