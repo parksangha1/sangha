0f0b44050593dd859f630358aee5c119ebccacd9
User <User@DESKTOP-P5G4SF9>
2023-04-17 23:15:26 +0900

    55

--- src/sangha/MenuManager.java
+++ src/sangha/MenuManager.java
@@ -6,7 +6,7 @@ public class MenuManager {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		VeicleOwnerManager veicleOwnerManager = new VeicleOwnerManager(input);
+		VehicleOwnerManager vehicleOwnerManager = new VehicleOwnerManager(input);
 		
 		int num=0;
 		while(num!=6) {
@@ -14,23 +14,23 @@ public class MenuManager {
 			System.out.println("1. Add Vehicle Owner");
 			System.out.println("2. Delete Vehicle Owner");
 			System.out.println("3. Edit Vehicle Owner");
-			System.out.println("4. View Vehicle Owner");
+			System.out.println("4. View Vehicle Owners");
 			System.out.println("5. Show a menu");
 			System.out.println("6. Exit");
 			System.out.println("Select one number between 1-6: ");
 			num=input.nextInt();
 			switch (num) {
 			case 1:
-				veicleOwnerManager.addVeicleOwner();
+				vehicleOwnerManager.addVehicleOwner();
 				break;
 			case 2:
-				veicleOwnerManager.deleteVeicleOwner();
+				vehicleOwnerManager.deleteVehicleOwner();
 				break;
 			case 3:
-				veicleOwnerManager.editVeicleOwner();
+				vehicleOwnerManager.editVehicleOwner();
 				break;
 			case 4:
-				veicleOwnerManager.viewVeicleOwner();
+				vehicleOwnerManager.viewVehicleOwners();
 				break;
 			case 5:
 				
