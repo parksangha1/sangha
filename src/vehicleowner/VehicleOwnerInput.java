8cd24d597ffb726a81be80cacbfc71c8a9f6dd08
User <User@DESKTOP-P5G4SF9>
2023-05-22 23:43:45 +0900

    5/22

--- src/vehicleowner/VehicleOwnerInput.java
+++ src/vehicleowner/VehicleOwnerInput.java
@@ -2,6 +2,8 @@ package vehicleowner;
 
 import java.util.Scanner;
 
+import exception.DepartmentFormatException;
+
 public interface VehicleOwnerInput {
 	
 	public int getNumber();
@@ -12,10 +14,17 @@ public interface VehicleOwnerInput {
 	
 	public void setType(String type);
 	
-	public void setDepartment(String department);
+	public void setDepartment(String department) throws DepartmentFormatException;
 	
 	public void getUserInput(Scanner input);
 	
 	public void printInfo();
 	
+	public void setVehicleNumber(Scanner input);
+	
+	public void setVehicleOwnerName(Scanner input);
+	
+	public void setVehicletype(Scanner input);
+	
+	public void setVehicleOwnerDepartment(Scanner input);
 }
