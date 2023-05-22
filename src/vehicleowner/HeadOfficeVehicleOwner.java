d94c8e17f42e163c2492dec2448bc3b3f9da4338
User <User@DESKTOP-P5G4SF9>
2023-05-22 23:42:42 +0900

    5/22

--- src/vehicleowner/HeadOfficeVehicleOwner.java
+++ src/vehicleowner/HeadOfficeVehicleOwner.java
@@ -2,44 +2,21 @@ package vehicleowner;
 
 import java.util.Scanner;
 
-public class HeadOfficeVehicleOwner extends VehicleOwner implements VehicleOwnerInput {
+public class HeadOfficeVehicleOwner extends VehicleOwner {
 	
 	public HeadOfficeVehicleOwner(VehicleOwnerKind kind) {
 		super(kind);
 	}
 	
 	public void getUserInput(Scanner input) {
-		System.out.print("Vehicle Owner Name : ");
-		String name = input.next();
-		this.setName(name);
-		
-		System.out.print("Vehicle's Number : ");
-		int number = input.nextInt();
-		this.setNumber(number);
-		
-		System.out.print("A type of car : ");
-		String type = input.next();
-		this.setType(type);
-		
-		System.out.print("Owner's Applicable Department : ");
-		String department = input.next();
-		this.setDepartment(department);
+		setVehicleOwnerName(input);
+		setVehicleNumber(input);
+		setVehicletype(input);
+		setVehicleOwnerDepartment(input);
 	}
 	
 	public void printInfo() {
-		String skind = "none";
-		switch(this.kind) {
-		case HeadOffice:
-			skind = "Head";
-			break;
-		case Guest:
-			skind = "Guest";
-			break;
-		case BranchOffice:
-			skind = "Branch";
-			break;
-		default:
-		}
+		String skind = getKindString();
 		System.out.println("kind: "+skind+" name: "+name+" number: "+number+" type: "+type+" department: "+department);
 	}
 }
