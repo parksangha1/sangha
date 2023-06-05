27d6f8a8e775f6498aab0e39918b653e1e4420d7
User <User@DESKTOP-P5G4SF9>
2023-06-06 00:36:58 +0900

    6-5

---
+++ src/gui/GuiTest.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class GuiTest {
+
+	public static void main(String[] args) {
+		//MenuSelection menuselection = new MenuSelection();
+		//VehicleOwnerAdder vehicleowneradder = new VehicleOwnerAdder();
+		VehicleOwnerViewer vehicleownerviewer = new VehicleOwnerViewer();
+	}
+
+}
